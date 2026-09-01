2f63ad8e557716994792b72b34e001442c8ec02c
MixterVee <mixtervandal@gmail.com>
2026-09-01 09:09:26 -0400

    Bump Fast Magnifier to v1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mixtervee.fastmagnifier"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.2"
+        versionCode = 8
+        versionName = "1.3"
     }
 
     buildFeatures {
