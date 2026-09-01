5406822ee65185521ed6355ec681d775b6bac2c5
MixterVee <mixtervandal@gmail.com>
2026-09-01 17:25:38 -0400

    Bump version to v1.3.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mixtervee.fastmagnifier"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.3.10"
+        versionCode = 19
+        versionName = "1.3.11"
     }
 
     buildFeatures {
