4249843510e3413176c63219672173e6f087a9a8
MixterVee <mixtervandal@gmail.com>
2026-08-31 21:15:21 -0400

    Bump portrait-only Full View build to v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mixtervee.fastmagnifier"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.1.1"
     }
 
     buildFeatures {
