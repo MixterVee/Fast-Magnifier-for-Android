d1bc6dfa8bdf3f6fffc02343ec7243f116141c6d
MixterVee <mixtervandal@gmail.com>
2026-09-12 15:40:46 -0400

    Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mixtervee.fastmagnifier"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.3.12"
+        versionCode = 21
+        versionName = "1.4.0"
     }
 
     buildFeatures {
